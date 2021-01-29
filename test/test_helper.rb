eed60f4f7e01955ef2d97991a1c706fa21ca111d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-01-29 11:18:44 +0900

    Fix release_test.rb on local (#32)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,9 @@ require "pathname"
 require "open3"
 
 module TestHelper
-  def sh!(cmd, env: {})
-    stdout, stderr, status = Open3.capture3(env, cmd)
+  def sh!(*cmd, env: {})
+    cmd = cmd.map { |arg| arg.respond_to?(:to_path) ? arg.to_path : arg }
+    stdout, stderr, status = Open3.capture3(env, *cmd)
     unless stdout.empty?
       puts "STDOUT --------------------"
       puts stdout
@@ -18,24 +19,24 @@ module TestHelper
       puts "---------------------------"
       puts
     end
-    status.success? or raise "Error: #{cmd}"
+    status.success? or raise "Error: #{cmd.join(' ')}"
     [stdout, stderr, status]
   end
 
-  def in_tmpdir(git: true)
+  def in_tmpdir(git: true, default_branch: "master")
     basedir = Pathname(__dir__) / ".."
 
     Dir.mktmpdir do |git_remote_dir|
       if git
         git_remote_path = Pathname(git_remote_dir) / "remote_repo.git"
-        sh! "git init --bare '#{git_remote_path}'"
+        sh! "git", "init", "--bare", "--initial-branch=#{default_branch}", git_remote_path
       end
 
       Dir.mktmpdir do |workdir|
         workdir = Pathname(workdir)
         Dir.chdir workdir
         if git
-          yield basedir, workdir, git_remote_path
+          yield basedir, workdir, git_remote_path, default_branch
         else
           yield basedir, workdir
         end
