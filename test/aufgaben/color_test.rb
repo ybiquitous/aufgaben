b98692882c07c358bd3272182ff694f1f4dffea9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-07-08 22:10:54 +0900

    Fix test on CI

--- test/aufgaben/color_test.rb
+++ test/aufgaben/color_test.rb
@@ -4,11 +4,13 @@ require_relative "../../lib/aufgaben/color"
 class ColorTest < Minitest::Test
   include TestHelper
 
-  def test_green
-    assert_equal "\e[32mhello\e[0m", Aufgaben::Color.new("hello").green
+  def test_green_with_enabled
+    Aufgaben::Color.stub :enabled?, true do
+      assert_equal "\e[32mhello\e[0m", Aufgaben::Color.new("hello").green
+    end
   end
 
-  def test_green_but_disabled
+  def test_green_with_disabled
     Aufgaben::Color.stub :enabled?, false do
       assert_equal "hello", Aufgaben::Color.new("hello").green
     end
